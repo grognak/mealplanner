454d99609edf1ce20261f63a6f7bae69ecb33344
Bill Lixfield <w.lixfield@gmail.com>
2025-06-05 17:39:12 -0400

    Configure NextAuth API

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { signIn } from "next-auth/react";
+import NextAuth from "next-auth";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label:"Username", type: "text" },
+                password: { label:"Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials || !credentials.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: { username: credentials.username },
+                });
+                if (!user) return null;
+
+                const valid = await bcrypt.compare(credentials?.password, user.password);
+                if (!valid) return null;
+
+                return { id: user.id, name: user.username, email: user.email };
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt" as const,
+    },
+    pages: {
+        signIn: "/login",
+        error: "/login",
+    },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
