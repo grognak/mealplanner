a5adee87ef8127e428957cd0ba0b2a26f6584b7a
Bill Lixfield <w.lixfield@gmail.com>
2025-06-06 14:38:28 -0400

    Add dummy test for husky to pass

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('Basic test', () => {
-  it('works', () => {
+describe("Basic test", () => {
+  it("works", () => {
     expect(true).toBe(true);
   });
 });
