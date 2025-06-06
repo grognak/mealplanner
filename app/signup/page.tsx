2da29e98ec0ade010d772a80d2aa0bde5f14851f
Bill Lixfield <w.lixfield@gmail.com>
2025-06-06 15:30:34 -0400

    feat: implement signup form with validation

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,120 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const signupFormSchema = z
+  .object({
+    username: z
+      .string()
+      .min(4, { message: "Username must be at least 4 characters." })
+      .max(50, { message: "Username must be less than 50 characters." }),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, { message: "The password must be at least 8 characters." }),
+    confirm: z.string().min(8),
+  })
+  .superRefine(({ confirm, password }, ctx) => {
+    if (confirm !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords do not match",
+        path: ["confirm"],
+      });
+    }
+  });
+
 export default function SignupPage() {
+  const form = useForm<z.infer<typeof signupFormSchema>>({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirm: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof signupFormSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>
-      <p>Meal Planner App</p>
-      <p>Sign Up Page</p>
+    <div className="max-w-md mx-auto mt-16 p-6 border rounded-2xl shadow-lg space-y-6">
+      <p className="text-2xl font-bold text-center">Meal Planner App</p>
+      <p className="text-xl font-bold text-center">Sign Up Page</p>
+
+      <div className="">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirm"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Confirm Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
