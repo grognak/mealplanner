21ea3e234fc03818c7f2f97de258f4273b45dc32
Bill Lixfield <w.lixfield@gmail.com>
2025-06-06 13:20:34 -0400

    Add a link to the login page on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <div>
       <p>Meal Planner App</p>
-      <Link href={"/signup"}>Sign Up</Link>
+      <>
+        <Link href={"/signup"}>Sign Up</Link>
+      </>
+      <br></br>
+      <>
+        <Link href={"/login"}>Login</Link>
+      </>
     </div>
   );
 }
