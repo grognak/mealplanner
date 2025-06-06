a8a049fe4e497c5337a9147e80254d14f5eb7790
Bill Lixfield <w.lixfield@gmail.com>
2025-06-06 13:15:31 -0400

    Add Link to signup page on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <p>Meal Planner App</p>
+      <Link href={"/signup"}>Sign Up</Link>
     </div>
   );
 }
