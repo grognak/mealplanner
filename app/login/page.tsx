e7506feb2379c50c64ba28b629541dc87124cd71
Bill Lixfield <w.lixfield@gmail.com>
2025-06-07 14:08:37 -0400

    feat: design login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,72 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { loginFormSchema } from "@/lib/validators/loginSchema";
+
 export default function LoginPage() {
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      usernameEmail: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>
-      <p>Meal Planner App</p>
-      <p>Login Page</p>
+    <div className="max-w-md mx-auto mt-16 p-6 border rounded-2xl shadow-lg space-y-6">
+      <p className="text-2xl font-bold text-center">Meal Planner App</p>
+      <p className="text-xl font-bold text-center">Login Page</p>
+
+      <div className="">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="usernameEmail"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username or Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Username or Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
