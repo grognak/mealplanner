b15a4584fe037c05ddcc7f016fd7211cd5d70507
Bill L. <w.lixfield@gmail.com>
2025-06-08 17:09:35 -0400

    [FEATURE] Add tabs to the dashboard (#5)
    * build: add shadcn/ui tabs dependency
    
    * feat: add tabs to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function DashboardPage() {
   const { data: session } = useSession();
@@ -11,6 +12,17 @@ export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard Page</p>
+      <Tabs
+        defaultValue="planner"
+        className="w-5/6 h-5/6 mx-8 border-4 border-zinc-400 rounded-2xl"
+      >
+        <TabsList>
+          <TabsTrigger value="planner">Meal Planner</TabsTrigger>
+          <TabsTrigger value="recipes">Recipe Box</TabsTrigger>
+        </TabsList>
+        <TabsContent value="planner">Meal Planner</TabsContent>
+        <TabsContent value="recipes">Recipe Box</TabsContent>
+      </Tabs>
     </div>
   );
 }
