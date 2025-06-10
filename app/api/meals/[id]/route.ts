1db75fc3269cc9dd3cae39fef1f60990fd4b3dd5
Bill L. <w.lixfield@gmail.com>
2025-06-10 22:39:47 +0000

    feat: add API route to delete meal

--- app/api/meals/[id]/route.ts
+++ app/api/meals/[id]/route.ts
@@ -32,3 +32,36 @@ export async function PATCH(
     });
   }
 }
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (!token?.id) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const meal = await prisma.meal.findFirst({
+      where: {
+        id: params.id,
+        userId: token.id,
+      },
+    });
+
+    if (!meal) {
+      return new Response("Meal not found", { status: 404 });
+    }
+
+    await prisma.meal.delete({
+      where: { id: params.id },
+    });
+
+    return new Response("Meal deleted", { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return new Response("Server error", { status: 500 });
+  }
+}
