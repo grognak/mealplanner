e62c31dcdd42eac87bf12112758c5aa5354c3b7b
Bill L. <w.lixfield@gmail.com>
2025-06-10 23:40:36 +0000

    feat: add search bar to recipe box

--- components/RecipeBox.tsx
+++ components/RecipeBox.tsx
@@ -25,6 +25,8 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export default function RecipeBox() {
   const { data: session, status } = useSession();
@@ -35,6 +37,8 @@ export default function RecipeBox() {
 
   const [selectedMeal, setSelectedMeal] = useState<MealFormData | null>(null);
   const [deleteMeal, setDeleteMeal] = useState<MealFormData | null>(null);
+
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     if (status === "authenticated" && session?.user.id) {
@@ -148,6 +152,25 @@ export default function RecipeBox() {
 
   return (
     <div className="recipe-box">
+      <div className="relative w-full max-w-full">
+        <Search
+          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+          size={18}
+        />
+        <Input
+          className="pl-10"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              console.log("Search Query: ", searchQuery);
+            }
+          }}
+          value={searchQuery}
+        />
+      </div>
+
       <div className="options-menu">
         <Button onClick={handleAddMeal}>Create Meal</Button>
       </div>
