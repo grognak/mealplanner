9c3bf9be9bbd39b316bc47061a396eca7570409a
Bill L. <w.lixfield@gmail.com>
2025-06-10 00:12:35 +0000

    refactor: remove demo meal card

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,16 +3,7 @@
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import MealCardComponent from "@/components/MealCard";
 import RecipeBox from "@/components/RecipeBox";
-
-import { Meal } from "@/types/custom";
-const testMeal: Meal = {
-  id: "001",
-  name: "Berry Smoothie",
-  tags: ["breakfast", "vegetarian", "quick"],
-  notes: ["Use different berry mixes"],
-};
 
 export default function DashboardPage() {
   const { data: session } = useSession();
@@ -22,7 +13,6 @@ export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard Page</p>
-      <MealCardComponent slug={testMeal} />
       <Tabs
         defaultValue="planner"
         className="w-5/6 h-5/6 mx-8 border-4 border-zinc-400 rounded-2xl"
